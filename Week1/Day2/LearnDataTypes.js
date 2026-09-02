503abfff699ab73cad2643b54eb4cbe13f0260f5
sajithpb <sajnair06@gmail.com>
2026-09-02 09:43:59 +0530

    Add files via upload
    Week1-Day1

---
+++ Week1/Day2/LearnDataTypes.js
@@ -0,0 +1,19 @@
+var firstName = 'Sajith'
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = 'Not Working'
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 9072978421
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlayWright
+console.log(hasPlayWright);
+console.log(typeof hasPlayWright);
